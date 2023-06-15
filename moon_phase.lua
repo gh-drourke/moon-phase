a1e9f38d570b1706d36ca457e76b286e1cef94e3
David Rourke <drourke82@gmail.com>
2023-06-15 13:54:58 -0700

    version: 0.0.2
    - feat: added moon illumination

--- moon_phase.lua
+++ moon_phase.lua
@@ -8,24 +8,28 @@ local function getMoonPhase()
 
     local moonPhase
     local moonAge
+    local moonIllumination
+    local moonSign
 
     local function handleResponse(data)
         -- Function to handle the response from the web page
         -- Extract the moon phase from the response
         moonPhase = data:match("Phase: (.-)<")
         moonAge = data:match("Moon age: (.-)<")
-
+        moonIllumination = data:match("Moon illumination: (.-)<")
+        moonSign = data:match("moon sign is (.-)<")
     end
 
     local easy = curl.easy()                            -- Create a new cURL easy handle
     easy:setopt(curl.OPT_URL, url)                      -- Set the URL to fetch
     easy:setopt(curl.OPT_WRITEFUNCTION, handleResponse) -- Set the callback function to handle the response
     easy:perform()                                      -- Perform the request and capture the response
-    easy:close()
-    return moonPhase, moonAge                                    -- Clean up the easy handle
+    easy:close()                                        -- Clean up the easy handle
+    return moonPhase, moonAge, moonIllumination, moonSign
 end
 
-local moonPhase, moonAge = getMoonPhase()
-print("Moon Phase:", moonPhase)
-print("Moon Age  :", moonAge)
--- getMoonPhase()
+local moonPhase, moonAge, moonIllumination, moonSign = getMoonPhase()
+print("Moon Phase         :", moonPhase)
+print("Moon Age           :", moonAge)
+print("Moon Illumination  :", moonIllumination)
+print("Moon Sign          :", moonSign)
