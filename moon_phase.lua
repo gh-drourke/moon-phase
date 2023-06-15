bb4c3283e4ef262c6113546c9ea66b6e76b02534
David Rourke <drourke82@gmail.com>
2023-06-15 13:31:19 -0700

    version: 0.0.1
    - feat: returning moon age as well

--- moon_phase.lua
+++ moon_phase.lua
@@ -1,20 +1,31 @@
-local curl = require "cURL"
+-- Function to scrape the web page and extract the moon phase
+local function getMoonPhase()
+    local curl = require "cURL"
 
--- URL of the web page containing the moon phase information
--- local url = "https://example.com/moonphase"
-local url =
-"https://nineplanets.org/moon/phase/today/#:~:text=The%20current%20moon%20phase%20for%20today%20is%20the%20Waning%20Crescent%20phase."
+    -- URL of the web page containing the moon phase information
+    local url =
+    "https://nineplanets.org/moon/phase/today/#:~:text=The%20current%20moon%20phase%20for%20today%20is%20the%20Waning%20Crescent%20phase."
 
--- Function to handle the response from the web page
-local function handleResponse(data)
-    -- Extract the moon phase from the response
-    local moonPhase = data:match("Phase: (.-)<")
+    local moonPhase
+    local moonAge
 
-    print("Moon Phase:", moonPhase)
+    local function handleResponse(data)
+        -- Function to handle the response from the web page
+        -- Extract the moon phase from the response
+        moonPhase = data:match("Phase: (.-)<")
+        moonAge = data:match("Moon age: (.-)<")
+
+    end
+
+    local easy = curl.easy()                            -- Create a new cURL easy handle
+    easy:setopt(curl.OPT_URL, url)                      -- Set the URL to fetch
+    easy:setopt(curl.OPT_WRITEFUNCTION, handleResponse) -- Set the callback function to handle the response
+    easy:perform()                                      -- Perform the request and capture the response
+    easy:close()
+    return moonPhase, moonAge                                    -- Clean up the easy handle
 end
 
-local easy = curl.easy()                            -- Create a new cURL easy handle
-easy:setopt(curl.OPT_URL, url)                      -- Set the URL to fetch
-easy:setopt(curl.OPT_WRITEFUNCTION, handleResponse) -- Set the callback function to handle the response
-easy:perform()                                      -- Perform the request and capture the response
-easy:close()                                        -- Clean up the easy handle
+local moonPhase, moonAge = getMoonPhase()
+print("Moon Phase:", moonPhase)
+print("Moon Age  :", moonAge)
+-- getMoonPhase()
